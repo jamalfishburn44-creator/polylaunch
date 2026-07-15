3155202107498dfc45838693bcb38d15e808eb25
jamalfishburn44-creator <jamalfishburn44@gmail.com>
2026-07-15 19:16:02 -0400

    Create PolyLaunchToken contract

--- contracts/PolyLaunchToken.sol
+++ contracts/PolyLaunchToken.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+contract PolyLaunchToken {
+
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+    address public creator;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply,
+        address _creator
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply;
+        creator = _creator;
+    }
+}
