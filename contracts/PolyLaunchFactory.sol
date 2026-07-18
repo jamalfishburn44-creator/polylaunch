8f19c3c72d1ea8ffc45250669c7bbc971dfde54c
jamalfishburn44-creator <jamalfishburn44@gmail.com>
2026-07-18 18:56:38 -0400

    Update PolyLaunchFactory.sol

--- contracts/PolyLaunchFactory.sol
+++ contracts/PolyLaunchFactory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "./PolyLaunchToken.sol";
+
 contract PolyLaunchFactory {
 
     address public owner;
@@ -11,7 +13,6 @@ contract PolyLaunchFactory {
     struct Project {
         address creator;
         address token;
-        address bondingCurve;
         string name;
         string symbol;
         uint256 supply;
@@ -25,7 +26,6 @@ contract PolyLaunchFactory {
         uint256 indexed projectId,
         address indexed creator,
         address token,
-        address bondingCurve,
         string name,
         string symbol
     );
@@ -47,5 +47,46 @@ contract PolyLaunchFactory {
 
     function setTreasury(address _newTreasury) external onlyOwner {
         treasury = _newTreasury;
+    }
+
+    function createProject(
+        string memory _name,
+        string memory _symbol,
+        uint256 _supply
+    ) external payable {
+
+        require(msg.value >= launchFee, "Launch fee not paid");
+        require(bytes(_name).length > 0, "Name required");
+        require(bytes(_symbol).length > 0, "Symbol required");
+        require(_supply > 0, "Invalid supply");
+
+        payable(treasury).transfer(msg.value);
+
+        PolyLaunchToken token = new PolyLaunchToken(
+            _name,
+            _symbol,
+            _supply,
+            msg.sender
+        );
+
+        totalProjects++;
+
+        projects[totalProjects] = Project({
+            creator: msg.sender,
+            token: address(token),
+            name: _name,
+            symbol: _symbol,
+            supply: _supply,
+            createdAt: block.timestamp,
+            graduated: false
+        });
+
+        emit ProjectCreated(
+            totalProjects,
+            msg.sender,
+            address(token),
+            _name,
+            _symbol
+        );
     }
 }
