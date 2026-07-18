02d372a481a25218c9ea8935a6bd2eab86c0354e
jamalfishburn44-creator <jamalfishburn44@gmail.com>
2026-07-18 18:51:16 -0400

    Add PolyLaunchToken contract

--- contracts/PolyLaunchToken.sol
+++ contracts/PolyLaunchToken.sol
@@ -2,21 +2,50 @@
 pragma solidity ^0.8.24;
 
 contract PolyLaunchToken {
-
     string public name;
     string public symbol;
+    uint8 public constant decimals = 18;
+
     uint256 public totalSupply;
+
+    address public factory;
     address public creator;
+
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    modifier onlyFactory() {
+        require(msg.sender == factory, "Only factory");
+        _;
+    }
 
     constructor(
         string memory _name,
         string memory _symbol,
-        uint256 _totalSupply,
+        uint256 _supply,
         address _creator
     ) {
         name = _name;
         symbol = _symbol;
-        totalSupply = _totalSupply;
         creator = _creator;
+        factory = msg.sender;
+
+        totalSupply = _supply;
+        balanceOf[_creator] = _supply;
+
+        emit Transfer(address(0), _creator, _supply);
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(to != address(0), "Invalid address");
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+
+        return true;
     }
 }
